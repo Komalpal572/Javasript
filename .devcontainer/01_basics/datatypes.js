5f80063b330456d266916c94d3012e0eddc1ece4
Komalpal572 <palkomal2003@gmail.com>
2026-03-17 17:18:53 +0000

    console.log("komal")

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat al js code as newer version
+// alert(3+3)  we are usiong nodejs not browser
+
+console.log(3
+    +3)//code readability should be high
+    console.log("komal")
+
+     /* number=> 2 to power 53
+     bigint for large number
+     string=>""
+     boolean=>standalone value
+     undefined=> not decalared yet
+    symbol=>unique mostly used in react
+    
