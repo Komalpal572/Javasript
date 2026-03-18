a3bb6df4b7aa57404296587ca373f45320b0ab34
Komalpal572 <palkomal2003@gmail.com>
2026-03-18 18:44:54 +0000

    mm

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -10,5 +10,21 @@ console.log(3
      string=>""
      boolean=>standalone value
      undefined=> not decalared yet
-    symbol=>unique mostly used in react
-    
+    symbol=>unique mostly used in react*/    
+
+    let score=33
+    console.log(typeof score)
+
+    let valueInNumber= Number(score)
+    console.log(valueInNumber)
+
+    //"33"=>33
+    //"33abc"=>NaN
+    //true=>1,False=>0
+
+    let isLoggedIn="komal"
+    let booleanisLoggedIn=Boolean(isLoggedIn);
+
+    //1=>true,0=>false
+    //""=>false
+    //"komal"=>true
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("komal")
+const accountId=14323
+let accountEmail="palkomal2003@gmail.com"
+var accountPassword="123445"
+accountcity="jaipur"
+let accountState;
+accountEmail="komal@gmail.com"
+accountPassword="23674464"
+accountcity="Kanpur"
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
